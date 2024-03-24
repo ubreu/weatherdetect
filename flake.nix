b477f7d4f848a0c379563b9ea4a477640703c5a8
Urs Breu <urs.breu@ergon.ch>
2024-03-24 13:55:06 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Tooling for Homebridge iBricks";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      #flake = { }; # system-agnostic flake attributes
+      systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux"];
+      imports = [inputs.devenv.flakeModule];
+      # Documentation of arguments: https://flake.parts/module-arguments.html
+      perSystem = {pkgs, ...}: {
+        # define development shells
+        devenv.shells.default = {config, ...}: {
+          packages = [ pkgs.google-cloud-sdk ];
+          languages.go = {
+            enable = true;
+          };
+
+          languages.terraform = {
+            enable = true;
+          };
+
+          # project specific environment variables or commands/aliases
+          env.PROJECT_NAME = "Rain Detect";
+          scripts.hello.exec = ''
+            cat <<EOF
+            ## Welcome to a devenv setup for '$PROJECT_NAME'
+               your repository is at DEVENV_ROOT [$DEVENV_ROOT]
+               nix dependencies are symlinked to $DEVENV_DOTFILE
+               available binaries: [$(cd $DEVENV_DOTFILE/profile/bin; echo *)]
+            EOF
+          '';
+        };
+      };
+    };
+}
